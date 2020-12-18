1ed8a201fd9e5d4d34c8f8af78e3e875c4dd2c5e
itsaymhh <amr.alshmmaa@gmail.com>
2020-12-19 02:47:27 +1100

    Update forLoop.py

--- forLoop.py
+++ forLoop.py
@@ -1,7 +1,31 @@
-grocery = ['bread', 'milk', 'cake', 'bikkies', 'custard', 'cereal']
+import time
 
-for food in grocery:
-    print(food)
-    break
+animals = ["turkey", "cow", "horse", "pig", "dog", "cat"]
 
-print(grocery[1])
+for animal in animals: 
+    print("A " + animal + " is the best")
+    if animal == "turkey":
+        print("gobble gobble")
+        time.sleep(1)
+
+    elif animal == "cow":
+        print("Moo Moo")
+        time.sleep(1)
+
+    elif animal == "horse":
+        print("Neigh")
+        time.sleep(1)
+
+    elif animal == "pig":
+        print("Oink Oink")
+        time.sleep(1)
+
+    elif animal == "dog":
+        print("Woof Woof")
+        time.sleep(1)
+
+    elif animal == "cat":
+        print("Nom Nom")
+        time.sleep(1)
+
+print("Just finsihed printing out all the animals and the sounds they produce.")
