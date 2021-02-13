9878d0492f700dcb4131af1ccba96d6426ae9c85
itsaymhh <68047389+aymhh@users.noreply.github.com>
2021-02-13 18:37:58 +1100

    Create binarySearch.py

---
+++ searching-alogrithms/binarySearch.py
@@ -0,0 +1,19 @@
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+def binarySearch(array, value):
+
+    arrayLength = len(array)
+
+    if arrayLength%2 == "0":
+        arrayMiddleValue = array[round(arrayLength/2)]
+        print(f"{arrayMiddleValue} this is the middle value if the array is even")
+    elif arrayLength%2 != "0":
+        arrayMiddleValue = array[round((arrayLength - 1)/2)]
+        print(f"{arrayMiddleValue} this is the middle value if the array is odd")
+
+    if value == arrayMiddleValue:
+        return print("found")
+    elif (arrayLength > 1):
+        print
